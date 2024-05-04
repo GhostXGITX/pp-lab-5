eee43b178b92bf74ead6e5ae26048cda90e5a5ed
Mikolaj <mikolaj.m.zulewski@gmail.com>
2024-05-04 11:55:58 +0200

    Utworzony nowy pakiet z plikiem i zmieniono metode Main

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.interfaces.Messenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +11,11 @@ public class Main {
             System.out.println("Name: " + person.getName());
             System.out.println("Age: " + person.getAge());
 
+            int result = MathUtils.add(10, 20);
+            System.out.println("Addition Result: " + result);
+
             Messenger messenger = new EmailMessenger();
-            messenger.sendMessage("test message");
+            messenger.sendMessage("test message and Addition result: " + result);
         } catch (InvalidAgeException e) {
             System.out.println("Invalid age: " + e.getMessage());
         }
